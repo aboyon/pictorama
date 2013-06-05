21ac591c229c1d766420dfac99bd67b7d4bebd31
David Silveira <jdsilveira@gmail.com>
2013-06-05 01:44:07 -0300

    Resized destination folder integrity verification

--- lib/picturama/album.rb
+++ lib/picturama/album.rb
@@ -6,6 +6,7 @@ module Picturama
     def initialize(args)
       @folder = args[:folder]
       @thumbnails = "#{args[:folder]}/thumbnails"
+      @resized = "#{args[:folder]}/resized"
     end
 
     def pictures(order = true)
@@ -35,6 +36,10 @@ module Picturama
       File.directory?(@thumbnails)
     end
 
+    def has_resized?
+      File.directory?(@resized)
+    end
+
     def has_broken_thumbnails?
       thumbnails_found = Dir["#{@thumbnails}/*.{#{Picturama::config['allowed_formats'].join(',')}}"].size
       !has_thumbnails? || thumbnails_found < count_pictures
@@ -43,6 +48,12 @@ module Picturama
     def init_thumbnails
       unless has_thumbnails?
         FileUtils.mkdir_p "#{@thumbnails}"
+      end
+    end
+
+    def init_resized
+      unless has_resized?
+        FileUtils.mkdir_p "#{@resized}"
       end
     end
 
